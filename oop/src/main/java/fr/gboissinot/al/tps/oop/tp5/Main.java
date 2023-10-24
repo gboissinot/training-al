73ce379eba7ba740a7444d3557fdc9017edbacd0
Grégory BOISSINOT <gregory.boissinot@gmail.com>
2023-10-24 11:48:44 +0200

    Fix somes issues

--- oop/src/main/java/fr/gboissinot/al/tps/oop/tp5/Main.java
+++ oop/src/main/java/fr/gboissinot/al/tps/oop/tp5/Main.java
@@ -5,8 +5,8 @@ import fr.gboissinot.al.tps.oop.tp5.logger.LoggerFactory;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-//        Sample reflexion
-//        final Constructor<?>[] constructors = ServiceLocator.class.getDeclaredConstructors();
+        //Sample reflexion for LoggerFactory with constructor
+//        final Constructor<?>[] constructors = LoggerFactory.class.getDeclaredConstructors();
 //        for (Constructor<?> constructor : constructors) {
 //            constructor.setAccessible(true);
 //            constructor.newInstance(new Object[0]);
